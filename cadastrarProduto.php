13c63bcce7cea30da1b87d5cedc5a20c95e5a583
santas-littlehelper <132072748+santas-littlehelper@users.noreply.github.com>
2023-04-28 10:46:33 -0300

    Add files via upload

---
+++ cadastrarProduto.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produto</title>
+</head>
+<body>
+    <form id="formProduto" method="post" action="">
+        Nome do Produto: <input type="text" id="nomeProd">
+        Descrição: <input type="text" id="descricao">
+        Código: <input type="text" id="codigo">
+        Preço unitário: <input type="text" id="precoUn">
+        <input type="button" value="Cadastrar">
+    </form>
+</body>
+</html>
