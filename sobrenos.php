e5c0520057b17634552ebc5f15895bbbbe974991
geoadr <134656380+geoadr@users.noreply.github.com>
2023-05-26 11:09:44 -0300

    Update and rename csssobrenos.html to sobrenos.php

---
+++ sobrenos.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+   <p style="color: hsl(0, 58%, 4%);" style="text-align: center;"style="font-size: 30px;" >Sobre nós</p>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+        body {
+  background-color: #270f04; 
+  background-image: url("caminho/para/imagem.jpg");
+  background-repeat: repeat; 
+  background-position: center center; 
+  background-size: cover; 
+}
+form {
+          margin: 15px;
+        }
+label {
+          font-weight: bold;
+        }
+
+        textarea {
+          width: 50%;
+          height: 600px;
+          margin-bottom: 20px;
+        }
+        input[type="submit"] {
+          padding: 10px 20px;
+          background-color: #ecc7ad;
+          color: rgba(2, 0, 0, 0.774);
+          border: none;
+          cursor: pointer;
+        }
+        input[type="submit"]:hover {
+          background-color: #000000;
+        }
+        p {
+    color: rgb(245, 236, 236);
+    font-size: 10px;
+  }
+
+  p {
+    background-color: #ebcece;
+  }
+  
+  p {
+    color: #f7e5e5;
+  }
+  p {
+    text-align: center;
+  }
+  p {
+    font-size: 30px;
+  }
+
+    </style>
+</head>
+<body>
+  <style>
+    .texto-reduzido {
+      font-size: 5px;
+    }
+  </style>
+  <style>
+    .texto-bordado {
+      border: 1px solid rgb(240, 209, 209);
+      padding: 10px;
+    }</style>
+  <div class="background-animation"></div>
+    <form><link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+            <style>
+             body {font-family: 'Lobster', sans-serif;
+              }
+              h1 {
+                font-family: 'Lobster', sans-serif;
+              }
+              p {
+                font-family: 'Lobster', sans-serif;
+              }
+          </style>
+          <h1 class="texto-reduzido">
+          <h1 class="texto-bordado">
+            <p style="background-color: #270f04;"> A cacau mistério possui 7 criadores , 
+          cada um com suas habilidades e conhecimentos que se unem para oferecer um produto de qualidade e uma experiência de compra única aos clientes.</p>
+
+        <p style="background-color: #270f04;">Ana Clara - A especialista em embalagens. 
+Ana Clara é uma designer gráfico que trabalha com embalagens de produtos há mais de 10 anos. Ela é a responsável pelo design das embalagens dos bombons, que além de serem visualmente atrativas, protegem os chocolates e garantem a qualidade do produto.
+         </p><p style="background-color: #270f04;">
+Emily- A mestre dos sabores.
+Emily é uma chocolatier experiente, que estudou na França e na Bélgica para aprimorar suas habilidades na criação de bombons artesanais. Ela é responsável por desenvolver novas receitas e combinações de sabores que fazem os bombons do site serem verdadeiras obras de arte comestíveis.
+         </p><p style="background-color: #270f04;">
+Geovana - A web designer.
+Geovana é uma web designer talentosa, que cria sites bonitos e funcionais. Ela é o responsável por construir e manter o site de bombons, garantindo que ele seja fácil de usar e ofereça uma experiência de compra agradável aos clientes.
+         </p><p style="background-color: #270f04;">
+Kalliny- A produtora de conteúdo.
+Kalliny é uma escritora experiente, que já trabalhou em diversas agências de publicidade. Ela é responsável por produzir o conteúdo do site de bombons, criando textos que descrevam os sabores e a história de cada bombom.
+          </p><p style="background-color: #270f04;">
+Katielle - A expert em marketing digital. 
+Katielle é uma especialista em marketing digital, com conhecimentos em SEO, redes sociais e marketing de conteúdo. Ela é responsável por promover o site de bombons nas mídias digitais e garantir que ele alcance o público certo.
+         </p><p style="background-color: #270f04;">
+Rayssa - A administradora. 
+Rayssa é a responsável por cuidar da gestão financeira e do planejamento estratégico do negócio. Com sua experiência em finanças e gestão empresarial, ela garante que o site de bombons seja gerenciado de forma eficiente e sustentável.
+         </p><p style="background-color: #270f04;">
+Thiago - O fotógrafo.
+Thiago é um fotógrafo profissional que se especializou em fotografia gastronômica. Ele é o responsável por capturar as imagens dos bombons para o site, garantindo que as fotos transmitam a beleza e a qualidade dos chocolates. </p>
+        </p>
+          </h1> </h1></form>
+         
+</body>
+</html>
