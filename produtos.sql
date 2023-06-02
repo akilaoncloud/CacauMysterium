a7e9b8a95e0f56a1fb76db8060441b3d7887ce19
santas-littlehelper <132072748+santas-littlehelper@users.noreply.github.com>
2023-06-02 11:23:40 -0300

    Update and rename produtos.sql to BDA.sql

--- produtos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE 'produtos' (
-    idprodutos int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nomeProd varchar(45) NOT NULL,
-    descricao varchar(45) NOT NULL,
-    codigo int NOT NULL,
-    codigo float NOT NULL
-);
