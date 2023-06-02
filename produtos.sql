95a7afe021e039f04b6d23c940cb9d626c46630e
kallinyester <95769818+kallinyester@users.noreply.github.com>
2023-06-02 11:21:26 -0300

    Update produtos.sql

--- produtos.sql
+++ produtos.sql
@@ -1,8 +1,7 @@
 CREATE TABLE 'produtos' (
-  'idprodutos' int NOT NULL AUTO_INCREMENT,
-  'nomeProd' varchar(45) NOT NULL,
-  'descricao' varchar(45) NOT NULL,
-  'codigo' int NOT NULL,
-  'precoUn' float NOT NULL,
-   PRIMARY KEY ('idprodutos')
-) 
+    idprodutos int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomeProd varchar(45) NOT NULL,
+    descricao varchar(45) NOT NULL,
+    codigo int NOT NULL,
+    codigo float NOT NULL
+);
