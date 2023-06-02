7af4ed792e63172961b2464173700d053fff1fa6
kallinyester <95769818+kallinyester@users.noreply.github.com>
2023-06-02 11:06:07 -0300

    Update and rename produtos (1).sql to produtos.sql

---
+++ produtos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE 'produtos' (
+  'idprodutos' int NOT NULL AUTO_INCREMENT,
+  'nomeProd' varchar(45) NOT NULL,
+  'descricao' varchar(45) NOT NULL,
+  'codigo' int NOT NULL,
+  'precoUn' float NOT NULL,
+   PRIMARY KEY ('idprodutos')
+) 
