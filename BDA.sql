32272bd0a46710140f038d65a7aa84c3f98633f6
santas-littlehelper <132072748+santas-littlehelper@users.noreply.github.com>
2023-06-02 11:30:03 -0300

    Update BDA.sql

--- BDA.sql
+++ BDA.sql
@@ -1,16 +1,16 @@
-CREATE TABLE 'produtos' (
+CREATE TABLE produtos (
     idprodutos int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nomeProd varchar(45) NOT NULL,
     descricao varchar(45) NOT NULL,
-    codigo int NOT NULL,
-    codigo float NOT NULL
+    codigo int NOT NULL UNIQUE
+    
 );
 
-CREATE TABLE 'usuarios' (
+CREATE TABLE usuarios (
   idusuarios int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nomeC varchar(45) NOT NULL,
   nomeUser varchar(45) NOT NULL,
   email varchar(45) NOT NULL,
   senha varchar(45) NOT NULL,
-  CPF varchar(45) NOT NULL
+  CPF varchar(45) NOT NULL UNIQUE
 );
