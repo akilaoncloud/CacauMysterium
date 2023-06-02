44843bcd49bbedfa84295c02fdf7dff512b5888b
raytheweird <132076749+raytheweird@users.noreply.github.com>
2023-06-02 11:11:17 -0300

    Update barrapesquisa.php

--- barrapesquisa.php
+++ barrapesquisa.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['txt_Busca'])) {
 $termoBusca = $_POST['txt_Busca'];
-$buscaDAO = new BuscaDao("localhost", "cacau", "usuario", "senha");
+$buscaProduto = new BuscaProduto("localhost", "cacau", "usuario", "senha");
 $resultados = $buscaDAO->buscar("termoBusca");
 
 foreach($resultado as $resultado){
