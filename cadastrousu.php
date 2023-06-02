7dc305f645dcd10755a1dd4499c21e320b2e666d
raytheweird <132076749+raytheweird@users.noreply.github.com>
2023-06-02 10:34:02 -0300

    cadastrousu.php

--- cadastrousu.php
+++ cadastrousu.php
@@ -1,35 +1,38 @@
 <?php
 
 class CadastroUsuario {
-    private $nomeCompleto;
-    private $nomeUsuario;
-    private $senha;
-    private $cpf;
+    public $nomeCompleto;
+    public $nomeUsuario;
+    public $senha;
+    public $cpf;
 
-    public function __construct($nomeCompleto, $nomeUsuario, $senha, $cpf) {
-        $this->nomeCompleto = $nomeCompleto;
-        $this->nomeUsuario = $nomeUsuario;
+    public function __construct($nomeC, $nomeUser, $email, $senha,$CPF) {
+        $this->nomeC = $nomeC;
+        $this->nomeUser = $nomeUser;
+        $this->email = $email;
         $this->senha = $senha;
-        $this->cpf = $cpf;
+        $this->CPF = $CPF;
     }
 
     public function cadastrar() {
         echo "Usuário cadastrado com sucesso.<br>";
-        echo "Nome completo: " . $this->nomeCompleto . "<br>";
-        echo "Nome de usuário: " . $this->nomeUsuario . "<br>";
+        echo "Nome completo: " . $this->nomeC . "<br>";
+        echo "Nome de usuário: " . $this->nomeUser . "<br>";
+        echo "Email: " . $this->email . "<br>";
         echo "Senha: " . $this->senha . "<br>";
-        echo "CPF: " . $this->cpf . "<br>";
+        echo "CPF: " . $this->CPF . "<br>";
     }
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
-    $nomeCompleto = $_POST["nome_completo"];
-    $nomeUsuario = $_POST["nome_usuario"];
+    $nomeC = $_POST["nomeC"];
+    $nomeUsuario = $_POST["nomeUser"];
+    $email = $_POST["email"];
     $senha = $_POST["senha"];
     $cpf = $_POST["cpf"];
 
-    $cadastroUsuario = new CadastroUsuario($nomeCompleto, $nomeUsuario, $senha, $cpf);
+    $cadastroUsuario = new CadastroUsuario($nomeC, $nomeUser, $email, $senha, $CPF);
 
     $cadastroUsuario->cadastrar();
 }
