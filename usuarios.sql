6be717af84b8c2ce0342cb00bc08900edd275e06
santas-littlehelper <132072748+santas-littlehelper@users.noreply.github.com>
2023-06-02 11:20:28 -0300

    Update usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -1,10 +1,8 @@
-CREATE TABLE 'usuarios' (
-  'idusuarios' int NOT NULL AUTO_INCREMENT,
-  'nomeC' varchar(45) NOT NULL,
-  'nomeUser' varchar(45) NOT NULL,
-  'email' varchar(45) NOT NULL,
-  'senha' varchar(45) NOT NULL,
-  'CPF' varchar(45) NOT NULL,
-  PRIMARY KEY ('idusuarios')
+CREATE TABLE usuarios (
+  idusuarios int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nomeC varchar(45) NOT NULL,
+  nomeUser varchar(45) NOT NULL,
+  email varchar(45) NOT NULL,
+  senha varchar(45) NOT NULL,
+  CPF varchar(45) NOT NULL
 );
-
