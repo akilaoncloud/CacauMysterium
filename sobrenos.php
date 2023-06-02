46cff9265b1125b25814a01d1e7c933173070fb4
kallinyester <95769818+kallinyester@users.noreply.github.com>
2023-06-02 11:08:51 -0300

    Update sobrenos.php

--- sobrenos.php
+++ sobrenos.php
@@ -1,138 +1,81 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <p style="color: hsl(0, 58%, 4%);" style="text-align: center;"style="font-size: 30px;" >Sobre nós</p>
+  <link rel="stylesheet" type="text/css" href="styles.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sobre nós</title>
+    <title>Sobre Nós</title>
     <style>
-
-        body 
-        {
-            background-color: #270f04;
+         @font-face {
+            font-family: 'SaintCarell';
+            src: url(SaintCarellClean_PERSONAL_USE_ONLY.otf) format('opentype');
+            font-size: 40px;
+            font-style: normal;
+        }
+        body {
+            background-color: #270f04; 
+            background-image: url("caminho/para/imagem.jpg");
+            background-repeat: repeat; 
             background-position: center center; 
             background-size: cover; 
+            font-family: 'SaintCarell';
         }
-        form 
-        {
-          margin: 15px;
+        textarea {
+            width: 50%;
+            height: 600px;
+            margin-bottom: 20px;
         }
-        label
-        {
-          font-weight: bold;
+        p {
+            color: rgb(245, 236, 236);
+            font-size: 10px;
+            text-align: center;
+            font-size: 30px;
         }
-
-        textarea 
-        {
-          width: 50%;
-          height: 600px;
-          margin-bottom: 20px;
+        .texto-reduzido {
+            font-size: 5px;
         }
-
-        p 
-        {
-          color: rgb(245, 236, 236);
-          font-size: 10px;
+    
+        .texto-bordado {
+            border: 1px solid rgb(240, 209, 209);
+            padding: 10px;
         }
-
-        p 
-        {
-          background-color: #ebcece;
-        }
-  
-        p
-        {
-          color: #f7e5e5;
-        }
-        p 
-        {
-          text-align: center;
-        }
-        p
-        {
-          font-size: 30px;
-        }
-
     </style>
 </head>
-
 <body>
-  <style>
-    .texto-reduzido
-     {
-      font-size: 5px;
-     }
-  </style>
+  <div class="background-animation"></div>
 
-  <style>
-    .texto-bordado
-     {
-      border: 1px solid rgb(240, 209, 209);
-      padding: 10px;
-     }
-    </style>
-
-  <div 
-    class="background-animation">
-  </div>
-    <form>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
 
-            <style>
-             body 
-             {
-                font-family: 'Lobster', sans-serif;
-             }
-              h1
-               {
-                font-family: 'Lobster', sans-serif;
-               }
-              p 
-               {
-                font-family: 'Lobster', sans-serif;
-               }
-          </style>
-
-          <h1 class="texto-reduzido">
-           <h1 class="texto-bordado">
-            <p style="background-color: #270f04;"> A Cacau Misteryum possui 7 criadores, 
-          cada um com suas habilidades e conhecimentos que se unem para oferecer um produto de qualidade e uma experiência de compra única aos clientes.
+        <h1 class="texto-reduzido">
+        <h1 class="texto-bordado">
+            <p style="background-color: #ebcece; color: hsl(0, 58%, 4%); text-align: center; font-size: 30px;" >Sobre nós</p>
+            <p> A cacau mistério possui sete criadores, cada um com suas habilidades e conhecimentos que se unem 
+            para oferecer um produto de qualidade e uma experiência de compra única aos clientes.</p>
+            <p>Ana Clara - A especialista em embalagens. <br>Ana Clara é uma designer gráfico que trabalha com embalagens de produtos há mais de dez anos. 
+            Ela é a responsável pelo design das embalagens dos bombons, que além de serem visualmente atrativas, 
+            protegem os chocolates e garantem a qualidade do produto.
             </p>
-
-            <p style="background-color: #270f04;">
-                 Ana Clara - A especialista em embalagens. 
-                 Ana Clara é uma designer gráfica que trabalha com embalagens de produtos há mais de 10 anos. Ela é a responsável pelo design das embalagens dos bombons, que além de serem visualmente atrativas, protegem os chocolates e garantem a qualidade do produto.
-            </p>
-            <p style="background-color: #270f04;">
-                Emily- A mestre dos sabores.
-                Emily é uma chocolatier experiente, que estudou na França e na Bélgica para aprimorar suas habilidades na criação de bombons artesanais. Ela é responsável por desenvolver novas receitas e combinações de sabores que fazem os bombons do site serem verdadeiras obras de arte comestíveis.
-            </p>
-            <p style="background-color: #270f04;">
-                Geovana - A web designer.
-                Geovana é uma web designer talentosa, que cria sites bonitos e funcionais. Ela é o responsável por construir e manter o site de bombons, garantindo que ele seja fácil de usar e ofereça uma experiência de compra agradável aos clientes.
-            </p>
-            <p style="background-color: #270f04;">
-                Kalliny- A produtora de conteúdo.
-                Kalliny é uma escritora experiente, que já trabalhou em diversas agências de publicidade. Ela é responsável por produzir o conteúdo do site de bombons, criando textos que descrevam os sabores e a história de cada bombom.
-            </p>
-            <p style="background-color: #270f04;">
-                Katielle - A expert em marketing digital. 
-                Katielle é uma especialista em marketing digital, com conhecimentos em SEO, redes sociais e marketing de conteúdo. Ela é responsável por promover o site de bombons nas mídias digitais e garantir que ele alcance o público certo.
-            </p>
-            <p style="background-color: #270f04;">
-                Rayssa - A administradora. 
-                Rayssa é a responsável por cuidar da gestão financeira e do planejamento estratégico do negócio. Com sua experiência em finanças e gestão empresarial, ela garante que o site de bombons seja gerenciado de forma eficiente e sustentável.
-            </p>
-            <p style="background-color: #270f04;">
-                Thiago - O fotógrafo.
-                Thiago é um fotógrafo profissional que se especializou em fotografia gastronômica. Ele é o responsável por capturar as imagens dos bombons para o site, garantindo que as fotos transmitam a beleza e a qualidade dos chocolates. </p>
-            </p>
-          </h1> 
+            <p>Emily- A mestre dos sabores.<br>Emily é uma chocolatier experiente, 
+            que estudou na França e na Bélgica para aprimorar suas habilidades na criação de bombons artesanais. 
+            Ela é responsável por desenvolver novas receitas e combinações de sabores que fazem os bombons do site 
+            serem verdadeiras obras de arte comestíveis.</p>
+            <p>Geovana - A web designer.<br>Geovana é uma web designer talentosa, que cria sites bonitos e funcionais. 
+            Ela é o responsável por construir e manter o site de bombons, 
+            garantindo que ele seja fácil de usar e ofereça uma experiência de compra agradável aos clientes.</p>
+            <p>Kalliny- A produtora de conteúdo.<br>Kalliny é uma escritora experiente, que já trabalhou em diversas agências de publicidade. 
+            Ela é responsável por produzir o conteúdo do site de bombons, criando textos que descrevam os sabores e a história de cada bombom.</p>
+            <p>Katielle - A expert em marketing digital.<br>Katielle é uma especialista em marketing digital, 
+            com conhecimentos em SEO, redes sociais e marketing de conteúdo. 
+            Ela é responsável por promover o site de bombons nas mídias digitais e garantir que ele alcance o público certo.</p>
+            <p>Rayssa - A administradora.<br>Rayssa é a responsável por cuidar da gestão financeira e do planejamento estratégico do negócio. 
+            Com sua experiência em finanças e gestão empresarial, ela garante que o site de bombons seja gerenciado de forma eficiente e sustentável.</p>
+            <p>Thiago - O fotógrafo.<br>Thiago é um fotógrafo profissional que se especializou em fotografia gastronômica. 
+            Ele é o responsável por capturar as imagens dos bombons para o site, 
+            garantindo que as fotos transmitam a beleza e a qualidade dos chocolates.</p>
+        </h1> 
         </h1>
-    </form>      
 </body>
 </html>
