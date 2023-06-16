4d24e7bade921e88451a032260eb307635c68bd7
kallinyester <95769818+kallinyester@users.noreply.github.com>
2023-06-16 10:10:54 -0300

    Update barra_pesquisa.php

--- barra_pesquisa.php
+++ barra_pesquisa.php
@@ -5,9 +5,9 @@ $termoBusca = $_POST['txt_Busca'];
 $buscaProduto = new BuscaProduto("localhost", "cacau", "usuario", "senha");
 $resultados = $buscaDAO->buscar("termoBusca");
 
-foreach($resultado as $resultado){
-    echo $resultado['coluna'] . "</br>";
-}
+    foreach($resultado as $resultado){
+        echo $resultado['coluna'] . "</br>";
+    }
 }
 
 ?>
