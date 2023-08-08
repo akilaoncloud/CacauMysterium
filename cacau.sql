fd36872df54ce796594800970e63dc9bce8b2a90
kallinyester <95769818+kallinyester@users.noreply.github.com>
2023-08-08 14:29:46 -0300

    Update cacau.sql

--- cacau.sql
+++ cacau.sql
@@ -3,7 +3,7 @@ CREATE DATABASE cacau;
 CREATE TABLE produtos (
     idprodutos int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nomeProd varchar(45) NOT NULL,
-    descricao varchar(45) NOT NULL,
+    descricao varchar(200) NOT NULL,
     codigo int NOT NULL
 );
 
@@ -18,7 +18,7 @@ CREATE TABLE usuarios (
 
 CREATE TABLE endereco (
     idendereco int not null AUTO_INCREMENT PRIMARY KEY,
-	endereco varchar(100) NOT NULL,
+    endereco varchar(100) NOT NULL,
     numero int,
     bairro varchar (30) NOT NULL,
     cidade varchar (50) NOT NULL,
@@ -36,4 +36,25 @@ CREATE TABLE cartao (
     endereco int NOT NULL,
     FOREIGN KEY (usuario) REFERENCES usuarios(idusuarios),
     FOREIGN KEY (endereco) REFERENCES endereco(idendereco)
+);
+
+CREATE TABLE cliente (
+    idcliente int NOT null auto_increment primary key,
+    fk_cliente_idusuario int not null,
+    fk_cliente_idprodutos int,
+    fk_cliente_idcartao int not null,
+    fk_cliente_idendereco int not null,
+    FOREIGN KEY (fk_cliente_idusuario) REFERENCES usuarios(idusuarios),
+    FOREIGN KEY (fk_cliente_idprodutos) REFERENCES produtos(idprodutos),
+    FOREIGN KEY (fk_cliente_idcartao) REFERENCES cartao(idcartao),
+    FOREIGN KEY (fk_cliente_idendereco) REFERENCES endereco(idendereco)
+);
+
+CREATE TABLE venda (
+    fk_venda_idcliente int not null,
+    fk_venda_idprodutos int not null,
+    statusProduto varchar(15) not null,
+    dataVenda date,
+    FOREIGN KEY (fk_venda_idcliente) REFERENCES cliente(idcliente),
+    FOREIGN KEY (fk_venda_idprodutos) REFERENCES produtos(idprodutos)
 );
