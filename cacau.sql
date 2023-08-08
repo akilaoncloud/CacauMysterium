bcaa385527c67038a84241975d261a4fb4758254
Thiago Alexandre <70294974+akilaoncloud@users.noreply.github.com>
2023-08-08 15:18:28 -0300

    update `cacau.sql`

--- cacau.sql
+++ cacau.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE cacau;
 
-CREATE TABLE produtos (
-    idprodutos int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE produto (
+    idproduto int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nomeProd varchar(45) NOT NULL,
     descricao varchar(200) NOT NULL,
     codigo int NOT NULL
 );
 
-CREATE TABLE usuarios (
-    idusuarios int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE usuario (
+    idusuario int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nomeC varchar(45) NOT NULL,
     nomeUser varchar(45) NOT NULL,
     email varchar(45) NOT NULL,
@@ -17,8 +17,8 @@ CREATE TABLE usuarios (
 );
 
 CREATE TABLE endereco (
-    idendereco int not null AUTO_INCREMENT PRIMARY KEY,
-	endereco varchar(100) NOT NULL,
+    idendereco int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    logradouro varchar(100) NOT NULL,
     numero int,
     bairro varchar (30) NOT NULL,
     cidade varchar (50) NOT NULL,
@@ -27,20 +27,21 @@ CREATE TABLE endereco (
 );
 
 CREATE TABLE cliente (
-	idcliente int NOT null auto_increment primary key,
-    fk_cliente_idusuario int not null,
-    fk_cliente_idendereco int not null,
-    coin int,
-    FOREIGN KEY (fk_cliente_idusuario) REFERENCES usuarios(idusuarios),
+    idcliente int NOT NULL auto_increment primary key,
+    fk_cliente_idusuario int NOT NULL,
+    fk_cliente_idvenda int,
+    fk_cliente_idendereco int NOT NULL,
+    FOREIGN KEY (fk_cliente_idusuario) REFERENCES usuario(idusuario),
+    FOREIGN KEY (fk_cliente_idvenda) REFERENCES venda(idvenda),
     FOREIGN KEY (fk_cliente_idendereco) REFERENCES endereco(idendereco)
 );
 
 CREATE TABLE venda (
-	idvenda int NOT null auto_increment primary key,
-	fk_venda_idcliente int not null,
-    fk_venda_idprodutos int not null,
-    statusProduto varchar(15) not null,
-    dataVenda date not null,
+    idvenda int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fk_venda_idcliente int NOT NULL,
+    fk_venda_idproduto int NOT NULL,
+    statusProduto varchar(15) NOT NULL,
+    dataVenda date NOT NULL,
     FOREIGN KEY (fk_venda_idcliente) REFERENCES cliente(idcliente),
-    FOREIGN KEY (fk_venda_idprodutos) REFERENCES produtos(idprodutos)
+    FOREIGN KEY (fk_venda_idproduto) REFERENCES produto(idproduto)
 );
